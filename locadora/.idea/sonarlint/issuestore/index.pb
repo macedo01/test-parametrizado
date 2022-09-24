6d696d2603d8e8396e9cc502025fbfd17befa2a4
Eduardo Donadon <eduardodonadon@pop-os.localdomain>
2022-09-24 10:18:37 -0300

    First tests

---
+++ locadora/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+Ž
+^src/main/java/br/ucsal/bes20222/testequalidade/locadora/dominio/enums/SituacaoVeiculoEnum.java,8/1/812b0bd19ff849174c1c385157390ab7943e81c0
+ƒ
+Ssrc/main/java/br/ucsal/bes20222/testequalidade/locadora/persistence/VeiculoDAO.java,2/8/28a6e453670e844db7b87f54319803699547a8f5
+|
+Lsrc/main/java/br/ucsal/bes20222/testequalidade/locadora/dominio/Veiculo.java,e/c/ec9870d4311f34e5f0619c291eea5d6626e40008
+
+Osrc/main/java/br/ucsal/bes20222/testequalidade/locadora/business/LocacaoBO.java,0/2/02103240f922875febd54bcc62ce9cc37d7362b8
+{
+Ksrc/main/java/br/ucsal/bes20222/testequalidade/locadora/dominio/Modelo.java,0/2/02782af6a1f825833c99ccba73d0d5c309dbf255
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+z
+Jsrc/test/java/br/ucsal/bes20222/testequalidade/locadora/LocacaoBOTest.java,7/3/738414b59a3886893d82e3c665e70796513578b5
--- locadora/src/test/java/br/ucsal/bes20222/testequalidade/locadora/LocacaoBOTest.java
+++ locadora/src/test/java/br/ucsal/bes20222/testequalidade/locadora/LocacaoBOTest.java
@@ -1,18 +1,88 @@
 package br.ucsal.bes20222.testequalidade.locadora;
 
+import br.ucsal.bes20222.testequalidade.locadora.business.LocacaoBO;
+import br.ucsal.bes20222.testequalidade.locadora.dominio.Modelo;
+import br.ucsal.bes20222.testequalidade.locadora.dominio.Veiculo;
+import br.ucsal.bes20222.testequalidade.locadora.dominio.enums.SituacaoVeiculoEnum;
+import br.ucsal.bes20222.testequalidade.locadora.exception.VeiculoNaoEncontradoException;
+import br.ucsal.bes20222.testequalidade.locadora.persistence.VeiculoDAO;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.*;
+
 /**
  * Testes para os métodos da classe LocacaoBO.
  * 
  * @author claudioneiva
  *
  */
-public class LocacaoBOTest {
+class LocacaoBOTest {
+
+	/**
+	 * Testar o cálculo do valor total de locação por 3 dias de 1 veículo fabricado
+	 * em 2015. 
+	 */
+	@Test
+	void testarCalculoValorTotalLocacao1Veiculo3Dias() throws VeiculoNaoEncontradoException {
+		Double expectedRentValue = 360.00;
+
+		VeiculoDAO dao = new VeiculoDAO();
+		Veiculo veiculo = new Veiculo();
+		veiculo.setPlaca("ABC-1234");
+		veiculo.setModelo(new Modelo("Model X"));
+		veiculo.setSituacao(SituacaoVeiculoEnum.DISPONIVEL);
+		veiculo.setAnoFabricacao(2015);
+		veiculo.setValorDiaria(150.00);
+
+		dao.insert(veiculo);
+
+		List<String> placas = Arrays.asList(
+				veiculo.getPlaca()
+		);
+
+		Double rentValue = new LocacaoBO(dao).calcularValorTotalLocacao(placas, 3, LocalDate.now());
+
+		Assertions.assertEquals(expectedRentValue, rentValue);
+	}
 
 	/**
 	 * Testar o cálculo do valor total de locação por 4 dias de 2 veículos fabricados
-	 * em 2015. 
+	 * em 2015.
 	 */
-	public void testarCalculoValorTotalLocacao1Veiculo3Dias() {
+	@Test
+	void testarCalculoValorTotalLocacao2Veiculos4Dias() throws VeiculoNaoEncontradoException {
+		Double expectedRentValue = 960.00;
+
+		VeiculoDAO dao = new VeiculoDAO();
+		Veiculo veiculo = new Veiculo();
+		veiculo.setPlaca("ABC-1234");
+		veiculo.setModelo(new Modelo("Model X"));
+		veiculo.setSituacao(SituacaoVeiculoEnum.DISPONIVEL);
+		veiculo.setAnoFabricacao(2015);
+		veiculo.setValorDiaria(150.00);
+
+		Veiculo veiculo2 = new Veiculo();
+		veiculo2.setPlaca("CBA-4321");
+		veiculo2.setModelo(new Modelo("Model Y"));
+		veiculo2.setSituacao(SituacaoVeiculoEnum.DISPONIVEL);
+		veiculo2.setAnoFabricacao(2015);
+		veiculo2.setValorDiaria(150.00);
+
+		dao.insert(veiculo);
+		dao.insert(veiculo2);
+
+		List<String> placas = Arrays.asList(
+				veiculo.getPlaca(),
+				veiculo2.getPlaca()
+		);
+
+
+		Double rentValue = new LocacaoBO(dao).calcularValorTotalLocacao(placas, 4, LocalDate.now());
+
+		Assertions.assertEquals(expectedRentValue, rentValue);
 	}
 
 }
